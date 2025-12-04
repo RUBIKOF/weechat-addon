bfb6226de85a200a75469655a20dec5224af80af
RUBIKOF <47043942+RUBIKOF@users.noreply.github.com>
2025-12-04 16:36:23 -0600

    Update run.sh

--- kiwiirc/run.sh
+++ kiwiirc/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "=== Kiwi IRC 20.05.24.1 ==="
-echo "Instalado desde paquete .deb oficial"
+echo "Extraído desde paquete .deb"
 echo "Web UI: http://[TU_IP]:8080"
-echo "Conecta a ZNC en: localhost:6667"
 exec nginx -g "daemon off;"
